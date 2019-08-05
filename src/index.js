1eb04392de0d70e9f2e66aebad42cc837d55213f
irikiou <215611352@qq.com>
2019-08-05 12:45:34 +0800

    '处理全局样式’

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// 导入antd-mobile组件库中样式
+import 'antd-mobile/dist/antd-mobile.css'
+
+//导入全局样式
+import './index.css'
 
 // 导入根组件
 import App from './App'
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Button } from 'antd-mobile'
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>这是首页</h1>
+        {/* 渲染组件 */}
+        <Button type="primary">登录</Button>
       </div>
     )
   }
